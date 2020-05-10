2cf4891b3414e14b3c66b8eed8e80735811a4734
Eiichiro Komatsu <eiichirokomatsu@me.com>
2020-05-10 16:48:23 +0200

    Files generated by PkgTemplates

---
+++ src/OmegaGrav.jl
@@ -0,0 +1,5 @@
+module OmegaGrav
+
+greet() = print("Hello World!")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using OmegaGrav
+using Test
+
+@testset "OmegaGrav.jl" begin
+    # Write your own tests here.
+end
