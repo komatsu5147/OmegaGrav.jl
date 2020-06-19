0a0bbb897dd3c71bd5322a28f351242b574c43e8
Eiichiro Komatsu <eiichirokomatsu@me.com>
2020-06-19 15:38:22 +0200

    simply codes by introducing "compute_pk_class.jl"

--- examples/PlotFigure2.jl
+++ examples/PlotFigure2.jl
@@ -1,42 +1,9 @@
 using OmegaGrav
-using PyCall
 using Dierckx
 using Plots, LaTeXStrings
 include("dodlnMh.jl")
-# %% Call the python wrapper for CLASS, `classy`, via PyCall
-classy = pyimport("classy")
-# Create an instance of the CLASS wrapper
-cosmo = classy.Class()
-# Create a dictionary of the cosmological parameters
-deg_ncdm = true # 3 massive neutrinos with degenerate mass?
-params = Dict(
-   "output" => "mPk",
-   "P_k_max_h/Mpc" => 100,
-   "z_pk" => 5,
-   "non linear" => "halofit",
-   "A_s" => 2.097e-9,
-   "n_s" => 0.9652,
-   "k_pivot" => 0.05,
-   "h" => 0.6737,
-   "omega_b" => 0.02233,
-   "omega_cdm" => 0.1198,
-   "N_ncdm" => 1,
-)
-if deg_ncdm
-   push!(params, "m_ncdm" => 0.02, "deg_ncdm" => 3, "N_ur" => 0.00641)
-   mν = params["m_ncdm"] * params["deg_ncdm"]
-else
-   push!(params, "m_ncdm" => 0.06, "N_ur" => 2.0328)
-   mν = params["m_ncdm"]
-end
-h0 = params["h"]
-Ωcb = (params["omega_b"] + params["omega_cdm"]) / h0^2
-Ων = mν / 93.14 / h0^2 #  Ωνh^2 = ∑mν / (93.14 eV)
-Ωm = Ωcb + Ων
-# Set the parameters and run the CLASS code
-cosmo.set(params)
-cosmo.compute()
-
+# %% Compute the matter power spectrum using CLASS
+include("compute_pk_class.jl")
 # %% Compute dΩgrav/dlnMh and dΩtherm/dlnMh at z = 0
 z = 0
 # Define functions to return linear and non-linear power spectra
--- examples/PlotFigure3.jl
+++ examples/PlotFigure3.jl
@@ -1,42 +1,9 @@
 using OmegaGrav
-using PyCall
 using CSV
 using Dierckx
 using Plots, LaTeXStrings
-# %% Call the python wrapper for CLASS, `classy`, via PyCall
-classy = pyimport("classy")
-# Create an instance of the CLASS wrapper
-cosmo = classy.Class()
-# Create a dictionary of the cosmological parameters
-deg_ncdm = true # 3 massive neutrinos with degenerate mass?
-params = Dict(
-   "output" => "mPk",
-   "P_k_max_h/Mpc" => 100,
-   "z_pk" => 5,
-   "non linear" => "halofit",
-   "A_s" => 2.097e-9,
-   "n_s" => 0.9652,
-   "k_pivot" => 0.05,
-   "h" => 0.6737,
-   "omega_b" => 0.02233,
-   "omega_cdm" => 0.1198,
-   "N_ncdm" => 1,
-)
-if deg_ncdm
-   push!(params, "m_ncdm" => 0.02, "deg_ncdm" => 3, "N_ur" => 0.00641)
-   mν = params["m_ncdm"] * params["deg_ncdm"]
-else
-   push!(params, "m_ncdm" => 0.06, "N_ur" => 2.0328)
-   mν = params["m_ncdm"]
-end
-h0 = params["h"]
-Ωcb = (params["omega_b"] + params["omega_cdm"]) / h0^2
-Ων = mν / 93.14 / h0^2 #  Ωνh^2 = ∑mν / (93.14 eV)
-Ωm = Ωcb + Ων
-# Set the parameters and run the CLASS code
-cosmo.set(params)
-cosmo.compute()
-
+# %% Compute the matter power spectrum using CLASS
+include("compute_pk_class.jl")
 # %% Compute `Ωgrav = Ωm * W / 2` and Ωtherm at redshifts of the data points
 d = CSV.read("data/d16_Omega_th_data.csv")
 nred = size(d)[1]
