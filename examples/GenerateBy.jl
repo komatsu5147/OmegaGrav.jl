4781664292246cdc6795a686e6008d75dfafca78
Eiichiro Komatsu <komatsu@mpa-garching.mpg.de>
2021-06-02 18:45:18 +0200

    add `t10MF` option for `by`

--- examples/GenerateBy.jl
+++ examples/GenerateBy.jl
@@ -3,11 +3,11 @@ using Dierckx
 using Tables, CSV
 # %% Compute the matter power spectrum using CLASS
 include("compute_pk_class.jl")
-# %% Compute the Compton-y-weighted halo bias, `b_y`, at seven redshifts
+# %% Compute the Compton-y-weighted halo bias, `by`, at seven redshifts
 # Reference: Equation (B1) of Chiang, Makiya, Ménard & Komatsu, arXiv:2006.14650
 redshift = [0, 0.3, 0.5, 0.7, 1, 1.3, 1.5]
 nred = length(redshift)
-b_y = zeros(nred)
+byt10MF, byt08MF = zeros(nred), zeros(nred)
 for ired = 1:nred
    z = redshift[ired]
    # Define functions to return linear baryon+CDM power spectrum
@@ -17,13 +17,14 @@ for ired = 1:nred
    # Spline interpolate in log(k)
    lnk = log(1e-4):0.05:log(100)
    pkcb = Spline1D(lnk, pkcb_class.(exp.(lnk)))
-   # %% Compute the Compton-y-weighted halo bias, `b_y`.
-   b_y[ired] = by(x -> pkcb(log(x)), z, Ωm, Ωcb, αp = 0.12)
+   # %% Compute the Compton-y-weighted halo bias, `by`, for two halo multiplicity functions.
+   byt08MF[ired] = by(x -> pkcb(log(x)), z, Ωm, Ωcb, αp = 0.12)
+   byt10MF[ired] = by(x -> pkcb(log(x)), z, Ωm, Ωcb, t10MF = true) # not specifying `αp` sets `αp = 0.12`
 end
 
 #%% Save table data to by.csv
-t = Tables.table([redshift b_y])
-header = ["z", "by"]
+t = Tables.table([redshift byt08MF byt10MF])
+header = ["z", "by(Tinker08)", "by(Tinker10)"]
 CSV.write("by.csv", t, header = header)
 
 # %% Clean CLASS (the equivalent of the struct_free() in the `main`
